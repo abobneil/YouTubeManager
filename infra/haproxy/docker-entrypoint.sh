4be410bd5a7b8faf96d0b218178bd68487be533e
Neil Chester <chester747@gmail.com>
2026-03-07 20:23:56 -0500

    Automate TLS cert bootstrap and normalize escaped bcrypt auth
    - add `cert-init` service to generate `infra/haproxy/certs/ytm.pem` on first startup
    - update HAProxy startup ordering to wait for cert initialization
    - unescape `$$` to `$` for `HAPROXY_BASIC_AUTH_PASSWORD_BCRYPT` in entrypoint
    - refresh README/local ops docs to describe auto cert generation and `.env` bcrypt escaping

--- infra/haproxy/docker-entrypoint.sh
+++ infra/haproxy/docker-entrypoint.sh
@@ -6,6 +6,8 @@ set -eu
 : "${HAPROXY_BASIC_AUTH_USER:?HAPROXY_BASIC_AUTH_USER is required}"
 : "${HAPROXY_BASIC_AUTH_PASSWORD_BCRYPT:?HAPROXY_BASIC_AUTH_PASSWORD_BCRYPT is required}"
 
+HAPROXY_BASIC_AUTH_PASSWORD_BCRYPT=$(printf '%s' "$HAPROXY_BASIC_AUTH_PASSWORD_BCRYPT" | sed 's/\$\$/\$/g')
+
 printf '%s\n' "$TRUSTED_CLIENT_CIDRS" | tr ',' '\n' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed '/^$/d' > /tmp/trusted_cidrs.acl
 
 envsubst '${EDGE_SHARED_SECRET} ${HAPROXY_BASIC_AUTH_USER} ${HAPROXY_BASIC_AUTH_PASSWORD_BCRYPT}' \
